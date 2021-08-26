9e92e7f5905f7643b729bc96030507569d7b6afc
Elkoke <81260519+elkokeOrtiz@users.noreply.github.com>
2021-08-26 17:16:34 -0500

    iniciando aprendizaje

---
+++ Js Array/concat/app.js
@@ -0,0 +1,6 @@
+const papas = ["Jorge Ortiz ", "Perla Luna "];
+const hijos = ["Violetta Ortiz "];
+const mascotas = ["Kinki ", "Angry "];
+
+const familia = papas.concat(hijos, mascotas); 
+document.getElementById("app").innerHTML = familia;
