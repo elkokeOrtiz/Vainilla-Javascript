e76b85414e3a8889679dc8adca93ba2c97fcf0ad
Elkoke <81260519+elkokeOrtiz@users.noreply.github.com>
2021-08-26 18:00:31 -0500

    update

---
+++ Js Array/constructor/app.js
@@ -0,0 +1,3 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+document.getElementById("app").innerHTML = fruits.constructor;
---
+++ Js Array/copyWhitin/app.js
@@ -0,0 +1,2 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("app").innerHTML = fruits.copyWithin(2,4);
